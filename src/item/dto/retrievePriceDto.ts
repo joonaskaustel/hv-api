4adb33c26f607225296e24b4c9024979b14a57ea
Joonas <joonas.kaustel@gmail.com>
2020-09-11 19:27:31 +0300

    fix regex :fire:

--- src/item/dto/retrievePriceDto.ts
+++ src/item/dto/retrievePriceDto.ts
@@ -2,6 +2,6 @@ import { IsNotEmpty, Matches } from 'class-validator';
 
 export class RetrievePriceDto {
     @IsNotEmpty()
-    @Matches(/https:\/\/www.hinnavaatlus.ee\/[0-9]*\/[a-zA-Z-0-9]*\//i)
+    @Matches(/https:\/\/www.hinnavaatlus.ee\/[0-9]*\/[a-zA-Z-0-9]*\/$/i)
     link: string;
 }
